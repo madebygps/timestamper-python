d4de41cc8daab287e43c1c1dba3d729e698bb828
Firdous2307 <124298708+Firdous2307@users.noreply.github.com>
2025-07-03 10:40:00 +0100

    added utility function to format timestamps.

---
+++ utils.py
@@ -0,0 +1,7 @@
+def format_timestamp(seconds: float) -> str:
+    hours, seconds = divmod(int(seconds), 3600)
+    minutes, seconds = divmod(seconds, 60)
+    if hours > 0:
+        return f"{hours:02d}:{minutes:02d}:{seconds:02d}"
+    else:
+        return f"{minutes:02d}:{seconds:02d}" 
